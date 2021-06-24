c7e632f87bfc3af19e0d9d315f8af370b0d38f90
Josh Levan <35366199+joshualevan@users.noreply.github.com>
2021-06-24 15:23:37 -0400

    background image working through JSON object

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import countries from './data/countries'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  //state
+  const [currentCountry, setCurrentCountry] = useState("The United States of America");
+  const [currentWitness, setCurrentWitness] = useState("")
+
+  //Find Country Data
+  const findCountryData = (key, state) => {
+    let value; 
+    countries.map(country => {
+      if(country.name === state){
+        value = country[key];
+      }
+    })
+    return value
+  };
+
+  //Style
+  const backgroundStyle = {
+    backgroundImage : `url(${findCountryData('background', currentCountry)})`,
+    width : '100%',
+    height : '100vh',
+    backgroundRepeat : 'no-repeat',
+    backgroundSize : 'cover',
+    backgroundPosition : 'fixed',
+  }
+
+  return(
+      <div style={backgroundStyle}>{findCountryData('contitnent', currentCountry)}</div>
+  )
 }
 
-export default App;
+export default App
---
+++ src/data/countries.js
@@ -0,0 +1,18 @@
+import { uuid } from 'uuidv4'
+
+const countries = [
+    {
+        "id" : uuid(),
+        "name" : "The United States of America",
+        "background" : "./artwork/backgrounds/united-states.jpg",
+        "flag" : "",
+        "abbreviation" : "USA",
+        "contitnent" : "North America",
+        "capital" : "Washington, D.C.",
+        "city" : "New York City",
+
+
+    }
+]
+
+export default countries
